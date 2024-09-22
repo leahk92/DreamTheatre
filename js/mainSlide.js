f8bcd8b22ec0aa36f79b35a4f65440fc104c0b33
Leah <leahkang92@gmail.com>
2024-09-22 15:37:20 +0900

    0922test

--- js/mainSlide.js
+++ js/mainSlide.js
@@ -21,3 +21,51 @@ $(function () {
       indicators.eq(activeSlideIndex).addClass("active");
    }
 });
+
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".indicator");
+let currentSlide = 1;
+
+// var manualNav = function (manual) {
+//    slides.forEach((slide) => {
+//       slide.classList.remove("active");
+//       btns.forEach((btn) => {
+//          btn.classList.remove("active");
+//       });
+//    });
+//    slides[manual].classList.add("active");
+//    btns[manual].classList.add("active");
+// };
+// btns.forEach((btn, i) => {
+//    btn.addEventListener("click", () => {
+//       manualNav(i);
+//       currentSlide = 1;
+//    });
+// });
+
+// 슬라이드 자동 넘김
+var repeat = function (activeClass) {
+   let active = document.getElementsByClassName("active");
+   let i = 1;
+
+   var repeater = () => {
+      setTimeout(function () {
+         [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove("active");
+         });
+         slides[i].classList.add("active");
+         btns[i].classList.add("active");
+         i++;
+
+         if (slides.length == i) {
+            i = 0;
+         }
+         if (i >= slides.length) {
+            return;
+         }
+         repeater();
+      }, 10000);
+   };
+   repeater();
+};
+repeat();
